606d6b30a8d7efd209e572e9c8f46ce68b116318
cfcortes <cfcortes75@gmail.com>
2020-12-01 10:56:02 -0600

    reverse

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ export default class App {
   sumarParesFor() {
     let suma = 0;
 
-    for(let i = 0; i<=20; i = i + 2) {
+    for(let i = 1; i<=20; i = i + 1) {
+      if(i%2 === 0) {
         suma = suma + i;
+      }
     }
     
     return suma;
@@ -14,3 +16,4 @@ export default class App {
 let app = new App();
 
 console.log(app.sumarParesFor());
+
